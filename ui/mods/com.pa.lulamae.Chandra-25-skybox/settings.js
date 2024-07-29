daa614528dfad8aaed8e4e99f4967aa40d1f37a7
Lula-Mae <39922985+Lula-Mae@users.noreply.github.com>
2024-07-29 19:24:48 -0400

    Add files via upload

---
+++ ui/mods/com.pa.lulamae.Chandra-25-skybox/settings.js
@@ -0,0 +1,11 @@
+try
+{
+    if (model && model.skyBoxes)
+    {
+        model.skyBoxes.push({text: 'Chandra 25 Skybox by Lula Mae', value: '/pa/sky/terrain/textures/com.pa.lulamae.Chandra-25-skybox/skybox.json'});
+    }
+}
+catch (e)
+{
+    console.trace(e);
+}
